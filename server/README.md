a80b12da5465264b816eb92cc1aef7fab3418b65
Michael Schieben <babe@rockitbaby.de>
2012-12-20 12:41:05 +0100

    improved usability :)

--- server/README.md
+++ server/README.md
@@ -3,8 +3,28 @@ Server
 
 a simple server for OS X that serves files from a directory
 
+Usage
+===
+
+Installation and first time usage:
 ```
+
 cd DIRECTORY_FROM_WHERE_TO_SERVE_FILES
 curl https://raw.github.com/preciousforever/design-studio-tools/master/server/server.sh > ./server.sh
 . server.sh
+
 ```
+
+Once installed:
+```
+
+cd DIRECTORY_FROM_WHERE_TO_SERVE_FILES
+. server.sh
+
+```
+
+Press [Ctrl] + C to quit
+
+Tip
+===
+Open Terminal, type `cd ` and than drag a folder from Finder to Terminal. Hit [Enter].
--- server/server.sh
+++ server/server.sh
@@ -1,5 +1,13 @@
   port="${1:-8000}"
-  # open "http://localhost:${port}/"
+  open "http://localhost:${port}/"
+  echo "Starting Server"
+  echo ""
+  echo "/----/----/"
+  echo ""
+  echo "Press [Ctrl] + C to quit"
+  echo ""
+  echo "/----/----/"
+  echo ""
   # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
   # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
   python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
